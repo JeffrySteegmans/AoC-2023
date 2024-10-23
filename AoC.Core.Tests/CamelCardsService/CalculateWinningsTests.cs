9b156bddc89d394f2730ea80cbf65eb71d99ec11
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2024-10-23 13:01:32 +0200

    Uncommited changes

---
+++ AoC.Core.Tests/CamelCardsService/CalculateWinningsTests.cs
@@ -0,0 +1,26 @@
+﻿using AoC.Core.CamelCards;
+
+namespace AoC.Core.Tests.CamelCardsServiceTests;
+
+public sealed class CalculateWinningsTests
+{
+    [Fact]
+    public void ShouldReturnCorrectWinnings()
+    {
+        var input = new List<string>
+        {
+            "32T3K 765",
+            "T55J5 684",
+            "KK677 28",
+            "KTJJT 220",
+            "QQQJA 483",
+        };
+        var expectedResult = 6440;
+
+        var service = new CamelCardsService(input);
+
+        var result = service.CalculateWinnings();
+
+        result.Should().Be(expectedResult);
+    }
+}
---
+++ AoC.Core/CamelCards/CamelCardsService.cs
@@ -0,0 +1,35 @@
+﻿namespace AoC.Core.CamelCards;
+
+public interface ICamelCardsService
+{
+    long CalculateWinnings();
+}
+
+public sealed class CamelCardsService(List<string> input)
+        : ICamelCardsService
+{
+    private readonly List<Game> _games = input
+            .Select(x => x.Split(' '))
+            .Select(x => new Game(x[0], int.Parse(x[1])))
+            .ToList();
+
+    public long CalculateWinnings()
+    {
+        var patternValue = _games
+            .Select(game =>
+            {
+                return (game.Hand
+                    .Select(ch => game.Hand.Count(x => x == ch))
+                    .Sum(), game);
+            })
+            .OrderByDescending(x => x.Item1)
+            .Select(x => x.game)
+            .ToList();
+
+        //TODO: Order games by ranking
+
+        throw new NotImplementedException();
+    }
+}
+
+public record Game(string Hand, int Bid);
